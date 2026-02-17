771baf62ee6521430927ca869464b02fa758ed1a
김태우 <taewoo080401@gmail.com>
2026-02-17 19:14:16 +0900

    fix: redirect page after login

--- src/features/auth/ui/LoginButton.tsx
+++ src/features/auth/ui/LoginButton.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 const LoginButton = () => {
   return (
     <button
-      onClick={() => signIn("github", { callbackUrl: "/" })}
+      onClick={() => signIn("github", { callbackUrl: "/profiles" })}
       className="w-full bg-surface text-text px-8 py-4 rounded-lg font-medium hover:bg-surface/80 transition-colors flex items-center justify-center gap-2 mt-4 cursor-pointer">
       <Github className="w-5 h-5" />
       Login with GitHub
