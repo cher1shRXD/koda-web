4b2cc6c29f724c30b78ae46627d9877f79712a46
김태우 <taewoo080401@gmail.com>
2026-02-17 19:09:49 +0900

    fix: small things

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
       <div className="w-1/2 h-full flex flex-col items-center justify-center gap-16">
         <h1 className="font-playpen font-bold text-3xl tracking-widest">
           Hi my{" "}
-          <span className="neon-sign">KODA <span className="text-primary">.</span></span>
+          <span className="neon-sign">KODA<span className="text-primary">.</span></span>
         </h1>
         <div className="w-full max-w-xl flex flex-col items-start gap-4">
           <h2 className="text-4xl font-semibold">Login</h2>
--- src/features/auth/ui/LoginButton.tsx
+++ src/features/auth/ui/LoginButton.tsx
@@ -7,7 +7,7 @@ const LoginButton = () => {
   return (
     <button
       onClick={() => signIn("github", { callbackUrl: "/" })}
-      className="w-full bg-surface text-white px-8 py-4 rounded-lg font-medium hover:bg-surface/80 transition-colors flex items-center justify-center gap-2 mt-4 cursor-pointer">
+      className="w-full bg-surface text-text px-8 py-4 rounded-lg font-medium hover:bg-surface/80 transition-colors flex items-center justify-center gap-2 mt-4 cursor-pointer">
       <Github className="w-5 h-5" />
       Login with GitHub
     </button>
